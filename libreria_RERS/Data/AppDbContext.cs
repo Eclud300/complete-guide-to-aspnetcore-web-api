dca2b87545f85d13d1f208ef69d14600f7d181c4
Eclud <eclud673@gmail.com>
2023-11-01 12:02:28 -0700

    123

---
+++ libreria_RERS/Data/AppDbContext.cs
@@ -0,0 +1,15 @@
+﻿using libreria_RERS.Data.Models;
+using Microsoft.EntityFrameworkCore;
+namespace libreria_RERS.Data
+{
+    public class AppDbContext:DbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options):base(options)
+            
+        {
+
+        }
+
+        public DbSet<Book> Books { get; set; } 
+    }
+}
--- libreria_RERS/Startup.cs
+++ libreria_RERS/Startup.cs
@@ -9,13 +9,16 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 
+
 namespace libreria_RERS
 {
     public class Startup
     {
+        public string ConnectionString { get; set; }
         public Startup(IConfiguration configuration)
         {
             Configuration = configuration;
+            ConnectionString = Configuration.GetConnectionString("DefaultConnectionString");
         }
 
         public IConfiguration Configuration { get; }
@@ -23,15 +26,25 @@ namespace libreria_RERS
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
-            services.AddRazorPages();
+            services.AddControllers();
+            services.AddSwaggerGen(c =>
+            {
+                c.SwaggerDoc("v1", new OpenApiInfo { Title = "libreria_RERS", Version = "v1" });
+            });
         }
+
+            
+            
+        
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
             if (env.IsDevelopment())
             {
+                app.UseSwagger();
                 app.UseDeveloperExceptionPage();
+                app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "libreria_RERS v1"));
             }
             else
             {
